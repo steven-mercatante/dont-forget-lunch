29d15351266ee2f9d30db64cba9301cae7dfa77e
steven-mercatante <arrrms@gmail.com>
2014-01-20 00:04:22 -0500

    Soft check on creds

--- main.py
+++ main.py
@@ -19,18 +19,22 @@ active_notifiers = [
 
 # Create a connection
 with requests.session() as c:
-	# # Login
-	# c.post('https://www.seamless.com/food-delivery/login.m', data=payload)
+	# Login
+	c.post('https://www.seamless.com/food-delivery/login.m', data=payload)
 
-	# # Grab order history
-	# request = c.get('https://www.seamless.com/OrderHistory.m?vendorType=1')
+	# Grab order history
+	request = c.get('https://www.seamless.com/OrderHistory.m?vendorType=1')
 
-	# # Parse order history HTML to grab last order date
-	# soup = BeautifulSoup(request.text)
-	# last_order_date = soup.find_all('td', class_='first date')[0].strong.text
+	# Parse order history HTML to grab last order date
+	soup = BeautifulSoup(request.text)
+	try:
+		last_order_date = soup.find_all('td', class_='first date')[0].strong.text
+		print last_order_date
+	except Exception as e:
+		# If last_order_date couldn't be grabbed, it's likely that the login 
+		# process didn't work - double check your credentials!
+		sys.exit()
 
-
-	last_order_date = 'f'
 	# Grab today's date in the same format Seamless uses so you can compare
 	today = '{dt.month}/{dt.day}/{dt.year}'.format(dt=datetime.datetime.now())
 
